be6a018234efc67aeeaacf4ce6729d05240a926c
tarun2008 <70888164+tarun2008@users.noreply.github.com>
2020-10-24 20:53:31 +0530

    Add files via upload

---
+++ Ubox.js
@@ -0,0 +1,18 @@
+class Ubox {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("orange");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
+  
